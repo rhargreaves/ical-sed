77d9145bb456714252c70edd39bd9367919ac78d
rhargreaves <kiyouta@gmail.com>
2016-07-18 22:53:52 +0100

    Refactor

--- app.js
+++ app.js
@@ -6,24 +6,34 @@ const fs = require('fs');
 const request = require('request');
 const ICalParser = require('cozy-ical').ICalParser;
 
+function transformCalendar(url, callback) {
+	request.get(url, (error, response, body) => {
+		if(error) {
+			callback(error, null);
+			return;
+		}
+
+		var parser = new ICalParser();
+		parser.parseString(body, function(err, cal) {
+			if(err) {
+				callback(err, null);
+				return;
+			}
+			var strCal = cal.toString();
+			callback(null, strCal);
+		});
+	});
+}
+
 app.get('/', (req, res) => {
 	res.send('Welcome');
 });
 
 app.get('/ical', (req, res) => {
 	var icalUrl = 'https://raw.githubusercontent.com/rhargreaves/ical-sed/master/test/data/one_event.ics';
-	request.get(icalUrl, (error, response, body) => {
-		if(error) {
-			console.log(error);
-			return;
-		}
-
-		var parser = new ICalParser();
-		parser.parseString(body, function(err, cal) {
-			var strCal = cal.toString();
-			res.setHeader('content-type', 'text/calendar');
-			res.send(strCal);
-		});
+	transformCalendar(icalUrl, function(err, strCal) {
+		res.setHeader('content-type', 'text/calendar');
+		res.send(strCal);
 	});
 });
 
