a0d1b0b3283c0fdf95206b8a4c0d334750781f06
rhargreaves <kiyouta@gmail.com>
2016-07-18 21:23:37 +0100

    Return fixed ical

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 
-
+const port = process.env.PORT || 3000;
 const app = exports.app = require('express')();
-
+const path = require('path');
 
 app.get('/', (req, res) => {
     res.send('Welcome');
 });
-app.listen();
+
+app.get('/ical', (req, res) => {
+	res.sendFile(path.join(__dirname, 'test/data/one_event.ics'));
+});
+
+app.listen(port);
--- test/acceptance.js
+++ test/acceptance.js
@@ -2,18 +2,27 @@ var request = require('supertest');
 var should = require('should');
 var app = require('../app').app;
 
-
 describe('Site', function() {
-  describe('GET', function() {
-    it('should return 200', function(done) {
-		request(app)
-			.get('/')
-			.expect(200)
-			.end(function(err,res) {
-				done();
-			});
-    });
-  });
+	describe('GET', function() {
+		it('should return 200', function(done) {
+			request(app)
+				.get('/')
+				.expect(200)
+				.end(function(err,res) {
+					done();
+				});
+		});
+
+		it('should return ical specified on query string', function(done) {
+			request(app)
+				.get('/ical')
+				.expect(200)
+				.end(function(err,res) {
+					res.text.should.startWith('BEGIN:VCALENDAR');
+					done();
+				});
+		});
+	});
 });
 
 
